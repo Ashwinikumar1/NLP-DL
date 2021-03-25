cf93ce686b08d039ede1619a9e0921fe16bcd0e4
Ashwinikumar1 <ashwinikumar081@gmail.com>
2021-03-25 15:11:49 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,36 +1,45 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-import time
-time.sleep(100)
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 import sys
 from random import random
 from operator import add
 
 from pyspark.sql import SparkSession
-from pyspark.sql.types import ArrayType, StructField, StructType, StringType, IntegerType
 
 
 if __name__ == "__main__":
     """
         Usage: pi [partitions]
     """
-    spark = SparkSession        .builder        .appName("PythonPi")        .getOrCreate()
-    data = [('Category A', 100, "https:// This is category A"),
-        ('Category B', 120, "@ ! This is category B"),
-        ('Category C', 150, "This is category C !!")]
-    schema = StructType([
-    StructField('Category', StringType(), True),
-    StructField('Count', IntegerType(), True),
-    StructField('Comment', StringType(), True)])
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
 
-    # Convert list to RDD
-    rdd = spark.sparkContext.parallelize(data)
+    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    n = 100000 * partitions
 
-    # Create data frame
-    rdd1 = spark.createDataFrame(rdd,schema)
-    rdd1.show()
+    def f(_):
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
+
     spark.stop()
-
